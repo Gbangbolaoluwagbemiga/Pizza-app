4c701abf28ed488385f0e6e0d3f9e4272700a058
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-06-09 23:37:52 +0100

    project pizza updated

--- src/index.js
+++ src/index.js
@@ -3,72 +3,61 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import pizzaData from './data';
 
-const pizzaApiData = [
-  {
-    name: 'Focaccia',
-    ingredients: 'Bread with italian olive oil and rosemary',
-    price: 6,
-    photoName: 'pizzas/focaccia.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Margherita',
-    ingredients: 'Tomato and mozarella',
-    price: 10,
-    photoName: 'pizzas/margherita.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Spinaci',
-    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
-    price: 12,
-    photoName: 'pizzas/spinaci.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Funghi',
-    ingredients: 'Tomato, mozarella, mushrooms, and onion',
-    price: 12,
-    photoName: 'pizzas/funghi.jpg',
-    soldOut: false,
-  },
-  {
-    name: 'Pizza Salamino',
-    ingredients: 'Tomato, mozarella, and pepperoni',
-    price: 15,
-    photoName: 'pizzas/salamino.jpg',
-    soldOut: true,
-  },
-  {
-    name: 'Pizza Prosciutto',
-    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
-    price: 18,
-    photoName: 'pizzas/prosciutto.jpg',
-    soldOut: false,
-  },
-];
-
-function Header() {}
+function Header() {
+  return (
+    <header className="header text-uppercase text-center">
+      <h1>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
 function Menu(props) {
   return (
-    <div className="col-6">
-      <div className="row">
+    <div className="col-md-6">
+      <div className="row my-4 ">
         <img src={props.obj.photoName} alt="pizza pics" className="col-6" />
-        <div className="col-6">
-          <h2 className="">{props.obj.name}</h2>
+        <div className="col-6 my-md-auto">
+          <h3 className="">{props.obj.name}</h3>
           <p className="">{props.obj.ingredients}</p>
-          <p>{props.obj.price}</p>
+          <span>{props.obj.soldOut ? 'SOLD OUT' : props.obj.price}</span>
         </div>
       </div>
     </div>
   );
 }
-function Footer() {}
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 10;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  return (
+    <footer className="footer text-center fw-bold h2 mt-5">
+      {isOpen ? (
+        <Order closeHour={closeHour} openHour={openHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
+    </footer>
+  );
+}
+function Order({closeHour, openHour}) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us or order
+        online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
 
 // Rendering all other components and display it to the user
 function App() {
   return (
-    <div className="row ">
+    <div className="container row mx-auto w-75">
       <Header />
       {pizzaData.map(data => (
         <Menu obj={data} key={data.name} />
